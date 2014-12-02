1384ee74b8e09f2abb8edff5e3d66d187373f19f
Yue Xu <imxuyue@users.noreply.github.com>
2014-12-01 21:03:43 -0500

    adding route('win15.html')

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from schedule_api import get_terms
+from schedule_api import get_terms, get_schools
 
 @app.route('/')
 def index():
@@ -11,3 +11,13 @@ def index():
         options['api_error'] = True
 
     return render_template('index.html', **options)
+
+@app.route('/win15')
+def win15():
+    options = {}
+    try:
+        options['schools'] = get_schools(2020)
+    except:
+        options['api_error'] = True
+
+    return render_template('win15.html', **options)
